68ef75fc41e2c12e75283f41c8b18dfd384ba2a8
gigabit101 <thegigabit101@gmail.com>
2016-10-16 19:40:55 +0100

    1.10 port

--- src/main/java/net/doubledoordev/mtrm/MTRMCommand.java
+++ src/main/java/net/doubledoordev/mtrm/MTRMCommand.java
@@ -1,6 +1,7 @@
 package net.doubledoordev.mtrm;
 
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
@@ -26,20 +27,21 @@ public class MTRMCommand extends CommandBase
     }
 
     @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+    {
+        if (sender instanceof EntityPlayer) ((EntityPlayer) sender).openGui(MineTweakerRecipeMaker.instance, 0, sender.getEntityWorld(), 0, 0, 0);
+    }
+
+    @Override
     public String getCommandUsage(ICommandSender p_71518_1_)
     {
         return "Step 4: PROFIT?";
     }
 
-    @Override
-    public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_)
-    {
-        return MinecraftServer.getServer().isSinglePlayer() || super.canCommandSenderUseCommand(p_71519_1_);
-    }
+//    @Override
+//    public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_)
+//    {
+//        return MinecraftServer.getServer().isSinglePlayer() || super.canCommandSenderUseCommand(p_71519_1_);
+//    }
 
-    @Override
-    public void processCommand(ICommandSender sender, String[] args)
-    {
-        if (sender instanceof EntityPlayer) ((EntityPlayer) sender).openGui(MineTweakerRecipeMaker.instance, 0, sender.getEntityWorld(), 0, 0, 0);
-    }
 }
--- src/main/java/net/doubledoordev/mtrm/gui/MTRMGuiHandler.java
+++ src/main/java/net/doubledoordev/mtrm/gui/MTRMGuiHandler.java
@@ -1,8 +1,8 @@
 package net.doubledoordev.mtrm.gui;
 
-import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
 
 /**
  * @author Dries007
